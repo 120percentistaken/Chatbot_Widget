3ec1cfbe38174c07f66646a11256681806f3c07b
no <53334539+120percentistaken@users.noreply.github.com>
2025-12-14 13:39:57 +0800

    created an ai chatbot widget

---
+++ chatbot_ai_widget.js
@@ -0,0 +1,2 @@
+<script> window.chtlConfig = { chatbotId: "7981364332" } </script>
+<script async data-id="7981364332" id="chtl-script" type="text/javascript" src="https://chatling.ai/js/embed.js"></script>
